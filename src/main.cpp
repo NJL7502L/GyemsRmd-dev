31771ce681fd5d6f18f5bf0f204b9d390ae2c184
NJL7502L <g.hana1211.tako@gmail.com>
2020-10-17 14:35:04 +0900

    とりあえずコピペしてコンパイルが通った

---
+++ src/main.cpp
@@ -0,0 +1,47 @@
+#include "Arduino.h"
+#include <MsTimer2.h>
+#include "../lib/FlexCAN_T4/CanBus/CanBus.h"
+
+#if defined(__IMXRT1062__)
+CanBus<CAN3, 10> &canbus = CanBus<CAN3>::getInstance();
+#endif
+#if defined(__MK20DX256__)
+CanBus<CAN0> &canbus = CanBus<CAN0>::getInstance();
+#endif
+
+const int ledPin = 13;
+
+uint8_t buf[8] = {};
+
+void timerInt() {
+  canbus.fetch();
+  while (canbus.available(0x40)) {
+    canbus.read(0x40, buf);
+    Serial.print("g ");
+    Serial.print(buf[0]);
+    Serial.print("\t");
+
+    buf[0]++;
+  }
+  canbus.stage(0x40, buf);
+  canbus.write();
+  Serial.print("s ");
+  Serial.print(buf[0]);
+  Serial.println();
+  digitalWrite(ledPin, !digitalRead(ledPin));
+}
+
+void setup() {
+  Serial.begin(2000000);
+  pinMode(ledPin, OUTPUT);
+
+  canbus.begin(1000000);
+
+  MsTimer2::set(1, timerInt);
+  MsTimer2::start();
+}
+
+void loop() {
+  // Serial.println(buf[0]);
+  delay(100);
+}
